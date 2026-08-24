6548de32755234afa4ff1235953b92b3d88ddd09
Shathy <100697385+Shathy@users.noreply.github.com>
2026-08-24 16:51:28 +0300

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // ==================== تسجيل الطلبات ====================
 app.use((req, res, next) => {
